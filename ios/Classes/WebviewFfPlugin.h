c8114bda394e728b6f7f778e8f6d58a6c540b024
tielan.fu <tielan.fu@chinacreator.com>
2019-01-28 09:19:33 +0800

    'first'

---
+++ ios/Classes/WebviewFfPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface WebviewFfPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/WebviewFfPlugin.m
@@ -0,0 +1,20 @@
+#import "WebviewFfPlugin.h"
+
+@implementation WebviewFfPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"webview_ff"
+            binaryMessenger:[registrar messenger]];
+  WebviewFfPlugin* instance = [[WebviewFfPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
