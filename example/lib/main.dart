32ac609530c49b705b9c2493cd875bc36da6be82
tielan.fu <>
2019-01-29 09:16:44 +0800

    a

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,166 +12,78 @@ class WebViewExample extends StatelessWidget {
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
 
+  void onItemClick(int i) async {
+    WebViewController webViewController = await _controller.future;
+    if (await webViewController.canGoBack()) {
+      webViewController.goBack();
+    } else {
+      print('back');
+    }
+    ;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView example'),
-        // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
-        actions: <Widget>[
-          NavigationControls(_controller.future),
-          SampleMenu(_controller.future),
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              InkWell(
+                onTap: () => onItemClick(1),
+                child: Container(
+                  height: 44.0,
+                  child: Center(
+                    child: Text('item1'),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () => onItemClick(1),
+                child: Container(
+                  height: 44.0,
+                  child: Center(
+                    child: Text('item1'),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () => onItemClick(1),
+                child: Container(
+                  height: 44.0,
+                  child: Center(
+                    child: Text('item1'),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () => onItemClick(1),
+                child: Container(
+                  height: 44.0,
+                  child: Center(
+                    child: Text('item1'),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Expanded(
+              child: WebView(
+                  initialUrl: 'https://www.baidu.com',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onEventChanged: (event){
+                      print(event);
+                  },
+                  onWebViewCreated: (WebViewController webViewController) {
+                    _controller.complete(webViewController);
+                  }))
         ],
       ),
-      body: WebView(
-        initialUrl: 'https://flutter.io',
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        },
-      ),
-      floatingActionButton: favoriteButton(),
-    );
-  }
-
-  Widget favoriteButton() {
-    return FutureBuilder<WebViewController>(
-        future: _controller.future,
-        builder: (BuildContext context,
-            AsyncSnapshot<WebViewController> controller) {
-          if (controller.hasData) {
-            return FloatingActionButton(
-              onPressed: () async {
-                final String url = await controller.data.currentUrl();
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text("Favorited $url")),
-                );
-              },
-              child: const Icon(Icons.favorite),
-            );
-          }
-          return Container();
-        });
-  }
-}
-
-enum MenuOptions {
-  evaluateJavascript,
-  toast,
-}
-
-class SampleMenu extends StatelessWidget {
-  SampleMenu(this.controller);
-  final Future<WebViewController> controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<WebViewController>(
-      future: controller,
-      builder:
-          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
-        return PopupMenuButton<MenuOptions>(
-          onSelected: (MenuOptions value) {
-            switch (value) {
-              case MenuOptions.evaluateJavascript:
-                _onEvaluateJavascript(controller.data, context);
-                break;
-              case MenuOptions.toast:
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('You selected: $value'),
-                  ),
-                );
-                break;
-            }
-          },
-          itemBuilder: (BuildContext context) => <PopupMenuItem<MenuOptions>>[
-                PopupMenuItem<MenuOptions>(
-                  value: MenuOptions.evaluateJavascript,
-                  child: const Text('Evaluate JavaScript'),
-                  enabled: controller.hasData,
-                ),
-                const PopupMenuItem<MenuOptions>(
-                  value: MenuOptions.toast,
-                  child: Text('Make a toast'),
-                ),
-              ],
-        );
-      },
-    );
-  }
-
-  void _onEvaluateJavascript(
-      WebViewController controller, BuildContext context) async {
-    final String result = await controller
-        .evaluateJavascript("document.body.style.backgroundColor = 'red'");
-    Scaffold.of(context).showSnackBar(
-      SnackBar(
-        content: Text('JavaScript evaluated, the result is: $result'),
-      ),
-    );
-  }
-}
-
-class NavigationControls extends StatelessWidget {
-  const NavigationControls(this._webViewControllerFuture)
-      : assert(_webViewControllerFuture != null);
-
-  final Future<WebViewController> _webViewControllerFuture;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<WebViewController>(
-      future: _webViewControllerFuture,
-      builder:
-          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
-        final bool webViewReady =
-            snapshot.connectionState == ConnectionState.done;
-        final WebViewController controller = snapshot.data;
-        return Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: !webViewReady
-                  ? null
-                  : () async {
-                      if (await controller.canGoBack()) {
-                        controller.goBack();
-                      } else {
-                        Scaffold.of(context).showSnackBar(
-                          const SnackBar(content: Text("No back history item")),
-                        );
-                        return;
-                      }
-                    },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_forward_ios),
-              onPressed: !webViewReady
-                  ? null
-                  : () async {
-                      if (await controller.canGoForward()) {
-                        controller.goForward();
-                      } else {
-                        Scaffold.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text("No forward history item")),
-                        );
-                        return;
-                      }
-                    },
-            ),
-            IconButton(
-              icon: const Icon(Icons.replay),
-              onPressed: !webViewReady
-                  ? null
-                  : () {
-                      controller.reload();
-                    },
-            ),
-          ],
-        );
-      },
     );
   }
 }
